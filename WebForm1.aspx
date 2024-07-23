5e5a94b987277ab44ebcd16dbace5754a9721125
Inshiya Shabbirbhai Ravat <insravat@gmail.com>
2024-07-23 23:34:59 +0530

    first commit

---
+++ WebForm1.aspx
@@ -0,0 +1,90 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="RegistrationForm.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Table runat="server">
+                <asp:TableRow>
+                    <asp:TableCell>Name : </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:TextBox runat="server" ID="Name"></asp:TextBox>
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell>
+                        Email : 
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:TextBox runat="server" ID="Email"></asp:TextBox>
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell>
+                        Mobile Number : 
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:TextBox runat="server" ID="Mobile"></asp:TextBox>
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell>
+                        Country : 
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:DropDownList runat="server" OnSelectedIndexChanged="Country_SelectedIndexChanged" ID="Country" AutoPostBack="true">
+                            <asp:ListItem>USA</asp:ListItem>
+                            <asp:ListItem>India</asp:ListItem>
+                        </asp:DropDownList>
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        State : 
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:DropDownList runat="server" OnSelectedIndexChanged="State_SelectedIndexChanged" ID="State" AutoPostBack="true">
+                           
+                        </asp:DropDownList>
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        City : 
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:DropDownList runat="server" ID="City"></asp:DropDownList>
+                    </asp:TableCell>
+                 </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell>
+                        Certificates : 
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:CheckBoxList runat="server" ID="Cbl">
+                            <asp:ListItem>IELTS</asp:ListItem>
+                            <asp:ListItem>NCAT</asp:ListItem>
+                            <asp:ListItem>GRE</asp:ListItem>
+                            <asp:ListItem>Summer Internship</asp:ListItem>
+                        </asp:CheckBoxList>
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell>
+                        Gender : 
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:RadioButtonList ID="Gender" runat="server">
+                            <asp:ListItem>Male</asp:ListItem>
+                            <asp:ListItem>Female</asp:ListItem>
+                        </asp:RadioButtonList>
+                    </asp:TableCell>
+                </asp:TableRow>
+            </asp:Table>
+            <asp:Button ID="btn" runat="server" Text="Submit" OnClick="btn_Click"/>
+            <asp:Label ID="lblresult" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RegistrationForm
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Country_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            State.Items.Clear();
+            if(Country.SelectedValue == "USA")
+            {
+                ListItem florida = new ListItem("Florida");
+                ListItem california = new ListItem("California");
+                ListItem LA = new ListItem("Los Angeles");
+                State.Items.Add(florida);
+                State.Items.Add(california);
+                State.Items.Add(LA);
+            }
+            else if (Country.SelectedValue == "India")
+            {
+                ListItem gujarat = new ListItem("Gujarat");
+                ListItem MP = new ListItem("Madhya Pradesh");
+                ListItem HP = new ListItem("Himachal Pradesh");
+                State.Items.Add(gujarat);
+                State.Items.Add(MP);
+                State.Items.Add(HP);
+            }
+        }
+
+        protected void State_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            City.Items.Clear();
+            if (State.SelectedValue == "Florida")
+            {
+                ListItem  miami= new ListItem("Miami");
+                ListItem orlando = new ListItem("Orlando");
+                ListItem KW = new ListItem("Key West");
+                City.Items.Add(miami);
+                City.Items.Add(orlando);
+                City.Items.Add(KW);
+            }
+            else if (State.SelectedValue == "California")
+            {
+                ListItem sf = new ListItem("San Francisco");
+                ListItem sd = new ListItem("San Diego");
+                ListItem ok = new ListItem("Oakland");
+                City.Items.Add(sf);
+                City.Items.Add(sd);
+                City.Items.Add(ok);
+            }
+            else if (State.SelectedValue == "Los Angeles")
+            {
+                ListItem sf = new ListItem("Mid-City");
+                ListItem sd = new ListItem("Silver Lake");
+                ListItem ok = new ListItem("Echo Park");
+                City.Items.Add(sf);
+                City.Items.Add(sd);
+                City.Items.Add(ok);
+            }
+            else if (State.SelectedValue == "Gujarat")
+            {
+                ListItem sf = new ListItem("Dahod");
+                ListItem sd = new ListItem("Rajkot");
+                ListItem ok = new ListItem("Ahmedabad");
+                City.Items.Add(sf);
+                City.Items.Add(sd);
+                City.Items.Add(ok);
+            }
+            else if (State.SelectedValue == "Madhya Pradesh")
+            {
+                ListItem sf = new ListItem("Bhopal");
+                ListItem sd = new ListItem("Ujjain");
+                ListItem ok = new ListItem("Indore");
+                City.Items.Add(sf);
+                City.Items.Add(sd);
+                City.Items.Add(ok);
+            }
+            else if (State.SelectedValue == "Himachal Pradesh")
+            {
+                ListItem sf = new ListItem("Manali");
+                ListItem sd = new ListItem("Shimla");
+                ListItem ok = new ListItem("Dharamshala");
+                City.Items.Add(sf);
+                City.Items.Add(sd);
+                City.Items.Add(ok);
+            }
+        }
+
+        protected void btn_Click(object sender, EventArgs e)
+        {
+            lblresult.Text = "Name: "+ Name.Text + "<br/>" + "Email: " + Email.Text + "<br/>" + "Phone No.: " + Mobile.Text +"<br/>" + "Country: " + Country.SelectedValue + "<br/>" + "State: " + State.SelectedValue + "<br/>" + "City: " + City.SelectedValue + "<br/>" + "Certifiates: " + Cbl.SelectedValue + "<br/>" + "Gender: " + Gender.SelectedValue;
+        }
+    }
+}
