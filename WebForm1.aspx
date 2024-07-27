0aa26565d844eeb9fe3f17b1d2e90dce2a20445c
Inshiya Shabbirbhai Ravat <insravat@gmail.com>
2024-07-27 12:53:00 +0530

    initial commit

---
+++ WebForm1.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="FileUpload.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload ID ="file" runat="server"  AutoPostBack="true"/>
+            <asp:Button ID="btn" runat="server" OnClick="btn_Click" Text="Upload"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FileUpload
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_Click(object sender, EventArgs e)
+        {
+            if (file.PostedFile.ContentType == "image/jpeg" && file.PostedFile.ContentLength > 50000)
+            {
+                Response.Write("File type : " + file.PostedFile.ContentType);
+                Response.Write("File size :" + file.PostedFile.ContentLength);
+                Response.Write(file.PostedFile.FileName);
+                file.PostedFile.SaveAs("rooted path" + file.PostedFile.FileName);
+            }
+            
+        }
+    }
+}
