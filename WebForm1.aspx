5d75f13a9a8294899ee666f608946be5dec78adc
Inshiya Shabbirbhai Ravat <insravat@gmail.com>
2024-08-12 22:06:12 +0530

    first commit

---
+++ WebForm1.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ShopTotalPrice.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:CheckBoxList runat="server" ID="cbl1" AutoPostBack="true" OnSelectedIndexChanged="cbl1_SelectedIndexChanged">
+                <asp:ListItem Value="10">Phone</asp:ListItem>
+                <asp:ListItem Value="20">Laptop</asp:ListItem>
+                <asp:ListItem Value="30">TV</asp:ListItem>
+                <asp:ListItem Value="40">Car</asp:ListItem>
+                <asp:ListItem Value="50">House</asp:ListItem>
+            </asp:CheckBoxList>
+            <asp:TextBox runat="server" Enabled="false" ID="tb1"></asp:TextBox>
+            <asp:Button runat="server" ID="btn1" OnClick="btn1_Click" Text="purchase"/>
+            <asp:Label runat="server" ID="lbl1" ></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ShopTotalPrice
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        int totalPrice = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn1_Click(object sender, EventArgs e)
+        {
+            lbl1.Text = "<br/><br/>";
+            foreach(ListItem item in cbl1.Items){
+                if (item.Selected)
+                {
+                    lbl1.Text += item.Text +  "<br/>";
+                }        
+            }
+            lbl1.Text += "<br/>" + "Total price: " + tb1.Text;
+        }
+
+        protected void cbl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (ListItem item in cbl1.Items)
+            {
+                if (item.Selected)
+                {
+                    totalPrice += Convert.ToInt16(item.Value);
+                }
+            }
+            tb1.Text = totalPrice.ToString();
+        }
+    }
+}
