593339633caff9334be86688a1240e0204153f67
Inshiya Ravat <insravat@gmail.com>
2024-09-24 15:13:57 +0530

    form to access web service

---
+++ WebForm1.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="AccessWebService.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="tb" runat="server" />
+            <asp:Button ID="btn" runat="server" Text="Get Student" OnClick="btn_Click" />
+            <asp:GridView ID="gv" runat="server"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using AccessWebService.localhost;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Security;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AccessWebService
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_Click(object sender, EventArgs e)
+        {
+            System.Net.ServicePointManager.ServerCertificateValidationCallback = (senderX
+                , certificate, chain, SslPolicyErrors) =>
+            { return  true; };
+            empService ews = new empService();
+            int id = Convert.ToInt32(tb.Text);
+            DataSet ds = ews.MyService(id);
+            gv.DataSource = ds;
+            gv.DataBind();
+        }
+    }
+}
